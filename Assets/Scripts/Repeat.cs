e3707f826e93d3bb771b8bfff522dce8c21abbfd
Brian Lauber <blauber@arclia.com>
2018-06-03 12:42:08 -0400

    Added variation of Repeat.Times that returns values

--- Assets/Scripts/Repeat.cs
+++ Assets/Scripts/Repeat.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 
 static class Repeat {
 
     public static void Times(int count, Action action) {
         for (int i = 0; i < count; ++i) {
             action();
+        }
+    }
+
+
+    public static IEnumerable<T> Times<T>(int count, Func<T> func) {
+        for (int i = 0; i < count; ++i) {
+            yield return func();
         }
     }
 }
