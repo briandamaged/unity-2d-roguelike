bb01ede7e135a77689f870e7533cc4affd219cf8
Brian Lauber <blauber@arclia.com>
2018-06-03 22:09:53 -0400

    Prototype for smooth movement
    I'm not really a fan of the way the tutorial structured the
    code.  So, I'm taking a fairly different approach.

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class Move {
+
+    public static IEnumerator Linear(Rigidbody2D rb, Vector2 dest, float time = 1f, Action onComplete = null) {
+        float inverseTime = 1f / time;
+
+        while(true) {
+            float remaining = (rb.position - dest).sqrMagnitude;
+            if(remaining < float.Epsilon) {
+                break;
+            }
+
+            Vector2 delta = Vector2.MoveTowards(rb.position, dest, inverseTime * Time.deltaTime);
+            rb.MovePosition(delta);
+
+            yield return null;
+        }
+
+        if(onComplete != null) {
+            onComplete();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb;
+
+    private bool moving;
+
+	// Use this for initialization
+	void Start () {
+        this.rb = this.GetComponent<Rigidbody2D>();
+        this.moving = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(!this.moving) {
+            int dx = 0;
+            int dy = 0;
+
+            bool tryMove = false;
+
+            if (Input.GetKeyDown(KeyCode.A)) {
+                --dx;
+                tryMove = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.D)) {
+                ++dx;
+                tryMove = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.S)) {
+                --dy;
+                tryMove = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.W)) {
+                ++dy;
+                tryMove = true;
+            }
+
+            if(tryMove) {
+
+                Vector2 dest = this.rb.position + new Vector2(dx, dy);
+                StartCoroutine(Move.Linear(this.rb, dest, 0.1f, () => { this.moving = false; }));
+
+                this.moving = true;
+
+            }
+
+
+        }
+
+
+	}
+}
